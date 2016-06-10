1ef320669119c220df05254ddedc83c3156b9a4e
watanany <s1170087@gmail.com>
2016-06-10 20:58:23 +0900

    clean:

--- ex17py/ex17.py
+++ ex17py/ex17.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sys import stderr
 
 D = {
     1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
