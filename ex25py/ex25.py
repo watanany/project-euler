e891a4c077a022e61780d8a4610b6318bc511b0e
watanany <s1170087@gmail.com>
2016-08-03 16:47:52 +0900

    clean: ex25.py

--- ex25py/ex25.py
+++ ex25py/ex25.py
@@ -2,11 +2,8 @@
 F = { 1: 1, 2: 1 }
 
 def f(n):
-    F[n - 1] = F.get(n - 1)
-    F[n - 2] = F.get(n - 2)
-
-    if F[n - 1] is None: F[n - 1] = f(n - 1)
-    if F[n - 2] is None: F[n - 2] = f(n - 2)
+    if F.get(n - 1) is None: F[n - 1] = f(n - 1)
+    if F.get(n - 2) is None: F[n - 2] = f(n - 2)
 
     F[n] = F[n - 1] + F[n - 2]
 
